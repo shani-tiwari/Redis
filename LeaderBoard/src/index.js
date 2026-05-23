ae21752ef05f033eae85753d5331773b26e93051
shani-tiwari <shanitiwari2021@gmail.com>
2026-05-23 08:38:25 +0530

    leaderboard initial commit

---
+++ LeaderBoard/src/index.js
@@ -0,0 +1,19 @@
+/**
+ * incr
+ * zincrby
+ * zrevrange
+ * zrevrank
+ */
+
+
+/**
+ * select, update, locking, transaction
+ */
+
+/** EndPoints
+ *  POST /leaderboard/:postId/view     ->   increment view count of a post
+
+ *  POST /leaderboard/score            ->   add points to a user score
+ *  GET /leaderboard/                  ->   get top 10 leaders
+ *  GET /leaderboard/:playerId/rank    ->   get rank of a user
+ */
